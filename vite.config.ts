ce6420de2db8c6ad943a7bd60eeba8a780c401b3
Ali Najafizadeh <a.najafizadeh@gmail.com>
2025-10-16 14:11:06 -0400

    Made chuncking configured

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+          mobx: ['mobx', 'mobx-react-lite', 'mobx-state-tree'],
+          radix: [
+            '@radix-ui/react-context-menu',
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-dropdown-menu',
+            '@radix-ui/react-slot',
+          ],
+          ui: ['lucide-react', 'class-variance-authority', 'clsx', 'tailwind-merge'],
+        },
+      },
+    },
+  },
 })
